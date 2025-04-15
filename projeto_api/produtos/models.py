1d7fa148bc5bd547cb1cc0bc5cf20fef4245e684
lucasdaniel666 <lucasdaniel.dev2024@gmail.com>
2025-04-15 13:54:29 -0300

    exercicios

--- projeto_api/produtos/models.py
+++ projeto_api/produtos/models.py
@@ -4,11 +4,12 @@ class Categoria(models.Model):
     nome = models.CharField(max_length=100)
     def __str__(self):
         return self.nome
+
 class Produto(models.Model):
-nome = models.CharField(max_length=100)
-descricao = models.TextField()
-preco = models.DecimalField(max_digits=10, decimal_places=2)
-categoria = models.ForeignKey(Categoria, related_name='produtos',
-on_delete=models.CASCADE)
-def __str__(self):
-return self.nome
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, related_name='produtos',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
